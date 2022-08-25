dd6492ec9743d67b73056d9b5db6b18a7e628b1c
ngocquy032 <QuyNNTH2208032@fpt.com.vn>
2022-08-25 12:03:09 +0700

    init repo

---
+++ qui3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int integer1;
+	int integer2;
+	int sum;
+	
+	printf("X\n");
+	scanf("%d", &integer1);
+	
+	
+	printf("Y\n");
+	scanf("%d", &integer2);
+	
+	sum = integer1 + integer2;
+	
+	 printf("tong gia tri yza: %d\n", sum);
+	 
+	 return 0;
+	 
+	 
+}
+
